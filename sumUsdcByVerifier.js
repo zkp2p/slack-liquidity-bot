86375c0a5ae238332e735f3046b61f73ccf673e7
Ben <66370595+unhappyben@users.noreply.github.com>
2026-01-08 08:45:44 +0100

    Add new payment methods to sumUsdcByVerifier

--- sumUsdcByVerifier.js
+++ sumUsdcByVerifier.js
@@ -19,7 +19,9 @@ const paymentMethodToPlatform = {
   '0x6aa1d1401e79ad0549dced8b1b96fb72c41cd02b32a7d9ea1fed54ba9e17152e': 'Zelle', // zelle-chase
   '0x4bc42b322a3ad413b91b2fde30549ca70d6ee900eded1681de91aaf32ffd7ab5': 'Zelle', // zelle-bofa
   '0x3ccc3d4d5e769b1f82dc4988485551dc0cd3c7a3926d7d8a4dde91507199490f': 'PayPal',
-  '0x62c7ed738ad3e7618111348af32691b5767777fbaf46a2d8943237625552645c': 'Monzo'
+  '0x62c7ed738ad3e7618111348af32691b5767777fbaf46a2d8943237625552645c': 'Monzo',
+  '0xd9ff4fd6b39a3e3dd43c41d05662a5547de4a878bc97a65bcb352ade493cdc6b': 'n26',
+  '0x5908bb0c9b87763ac6171d4104847667e7f02b4c47b574fe890c1f439ed128bb': 'chime'
 };
 
 function delay(ms) {
